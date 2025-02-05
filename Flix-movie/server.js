039f03714c71a3617d0c198da9017103c4937a49
jeroen <jeroen_van_rijsselt@live.nl>
2025-02-05 10:05:42 +0100

    last normal way

--- Flix-movie/server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-const port = 3002;
-
-app.use(express.static("./"));
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
